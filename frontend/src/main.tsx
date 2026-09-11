cfefc193f9e760aeba0cee0085eeb47894c65b64
Alexsander Uglin <uglin76@mail.ru>
2026-09-11 14:02:41 +0300

    Подключить переключатель темы и обновлённые стили

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { ThemeToggle } from "./ThemeToggle";
 import "./styles.css";
 import "./ux.css";
+import "./theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ThemeToggle />
     <App />
   </React.StrictMode>,
 );
