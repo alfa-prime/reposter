38769eb1f377c7939805ca4a6a4fbf928df95e09
Alexsander Uglin <uglin76@mail.ru>
2026-09-11 10:54:10 +0300

    Зарегистрировать новые модели очереди

--- src/news_reposter/db/models/__init__.py
+++ src/news_reposter/db/models/__init__.py
@@ -1,11 +1,18 @@
 """ORM-модели приложения."""
 
 from news_reposter.db.models.attachment import PostAttachment
-from news_reposter.db.models.enums import AttachmentType, PostStatus, PublicationStatus
+from news_reposter.db.models.enums import (
+    AttachmentType,
+    PostStatus,
+    PublicationStatus,
+    QueueItemStatus,
+)
 from news_reposter.db.models.post import Post
 from news_reposter.db.models.publication import Publication
+from news_reposter.db.models.queue_item import QueueItem
 from news_reposter.db.models.source import Source
 from news_reposter.db.models.target import Target
+from news_reposter.db.models.target_source import TargetSource
 
 __all__ = [
     "AttachmentType",
@@ -14,6 +21,9 @@ __all__ = [
     "PostStatus",
     "Publication",
     "PublicationStatus",
+    "QueueItem",
+    "QueueItemStatus",
     "Source",
     "Target",
+    "TargetSource",
 ]
