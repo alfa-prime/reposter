4f52a81deab2a5ee3f1ee1baf40e24f9ba0561c7
Alexsander Uglin <uglin76@mail.ru>
2026-09-11 11:12:31 +0300

    Экспортировать репозиторий очереди

--- src/news_reposter/repositories/__init__.py
+++ src/news_reposter/repositories/__init__.py
@@ -1,5 +1,9 @@
 """Операции чтения и записи данных приложения."""
 
+from news_reposter.repositories.queue_item import (
+    QueueItemAlreadyExistsError,
+    QueueItemRepository,
+)
 from news_reposter.repositories.source import SourceAlreadyExistsError, SourceRepository
 from news_reposter.repositories.target import TargetAlreadyExistsError, TargetRepository
 from news_reposter.repositories.target_source import (
@@ -8,6 +12,8 @@ from news_reposter.repositories.target_source import (
 )
 
 __all__ = [
+    "QueueItemAlreadyExistsError",
+    "QueueItemRepository",
     "SourceAlreadyExistsError",
     "SourceRepository",
     "TargetAlreadyExistsError",
