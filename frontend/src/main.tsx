a61d399b4367b8902a266955557ad252dc84b404
Alexsander Uglin <uglin76@mail.ru>
2026-09-14 11:31:49 +0300

    Remove legacy frontend enhancer imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,17 +5,14 @@ import { ThemeToggle } from "./ThemeToggle";
 import "./styles.css";
 import "./ux.css";
 import "./theme.css";
-import "./queueEnhancements.css";
 import "./polish.css";
 import "./brand.css";
 import "./mediaControls.css";
 import "./queueExperiencePatch.css";
 import "./targetComponents.css";
 import "./videoEnhancements.css";
-import "./publicationActionsEnhancements.css";
 import "./aboutEnhancements.css";
 import "./sourceComponents.css";
-import "./brandEnhancements";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
