c46f0aaaf612972b099bb8b319e1ec56969a33f2
Alexsander Uglin <uglin76@mail.ru>
2026-09-14 13:44:26 +0300

    Initialize theme without floating settings control

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { ThemeToggle } from "./ThemeToggle";
+import { initializeTheme } from "./theme";
 import "./styles.css";
 import "./ux.css";
 import "./theme.css";
@@ -11,9 +11,10 @@ import "./mediaControls.css";
 import "./targetComponents.css";
 import "./sourceComponents.css";
 
+initializeTheme();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeToggle />
     <App />
   </React.StrictMode>,
 );
